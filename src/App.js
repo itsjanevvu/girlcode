a193e1bbb6d81a751c39e076868aaebd72fb9457
itsjanevvu <janewu152@gmail.com>
2022-11-12 08:32:33 -0500

    react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import { Route } from "react-router-dom";
+import Signin from './components/signin';
+import Filter from './components/filtercauses';
+import Display from './components/displaycauses';
+import Match from './components/matchpage';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
+
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          <Route path= "/" element= {<Signin/>} />
+          <Route path= "/filters" element= {<Filter/>} />
+          <Route path= "/display" element= {<Display/>} />
+          <Route path= "/match" element= {<Match/>}/>
+
+        </Routes>
+  
+    
+    
       </header>
     </div>
+
+    </Router>
   );
 }
 
